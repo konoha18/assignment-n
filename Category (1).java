c09e65c9d026efc148531ede2787dade95967e30
Prajakta Badade <69361359+hinata1826@users.noreply.github.com>
2022-05-27 19:17:08 +0530

    Update Category (1).java

--- Category (1).java
+++ Category (1).java
@@ -62,7 +62,7 @@ public class Category extends Vehicle implements NewInterface {
         else
             this.x = x - 1;
 
-        System.out.println("X and Y are different!!");
+        System.out.println("X and Y are different");
         return false;
     }
 
