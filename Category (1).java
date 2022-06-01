f0ed55337f43f24ea17479440e2a2bf006ccdb7e
Prajakta Badade <69361359+Prajakta-Badade@users.noreply.github.com>
2022-06-01 21:55:10 +0530

    Update Category (1).java

--- Category (1).java
+++ Category (1).java
@@ -6,7 +6,6 @@ import java.lang.*;
 
 public void logSargResult(int stripeIx, boolean[] rgsToRead)
 {
-
   for (int i = 0, valOffset = 0; i < elements; ++i, valOffset += 64) {
     long val = 0;
     for (int j = 0; j < 64; ++j) {
