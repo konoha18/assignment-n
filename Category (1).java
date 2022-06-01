239fbeb2c4cc15b8d81d606d4acd68b3305a6aa2
Prajakta Badade <69361359+Prajakta-Badade@users.noreply.github.com>
2022-06-01 21:50:17 +0530

    Update Category (1).java

--- Category (1).java
+++ Category (1).java
@@ -4,7 +4,21 @@ import javax.ejb.EJBException;
 import javax.ejb.RemoveException;
 import java.lang.*;
 
+public void logSargResult(int stripeIx, boolean[] rgsToRead)
+{
 
+  for (int i = 0, valOffset = 0; i < elements; ++i, valOffset += 64) {
+    long val = 0;
+    for (int j = 0; j < 64; ++j) {
+      int ix = valOffset + j;
+      if (rgsToRead.length == ix) break;
+      if (!rgsToRead[ix]) continue;
+      val = val | (1 << j);         
+    }
+
+  }
+
+}
 public class Category extends Vehicle implements NewInterface {
 
     String category;
